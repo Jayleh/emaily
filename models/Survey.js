2a2607a489e3a9336a6e248229f419932381664f
Jayleh <jaylehyun@gmail.com>
2018-12-24 23:34:40 -0800

    completed mailing backend, todo: mailing form frontend

--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
-  body: String,
   subject: String,
-  recipients: [String], // array with strings
+  body: String,
+  recipients: [RecipientSchema], // array of subdocument collection
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }, // every server is going to belong to a particular user (relationship field)
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-const User = mongoose.model('users');
+const User = mongoose.model('users'); // sidestep issue of running tests instead of directly requiring in model
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
